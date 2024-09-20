b09aa4b07efb1719cfade5bcb337aa7ca6549585
mozaiclink <54782171+mozaiclink@users.noreply.github.com>
2024-09-20 15:58:17 +0200

    index

---
+++ notesdecours/index.php
@@ -0,0 +1,113 @@
+<?php  include 'entete.php';?> <p class="a rotatedFloralHeartBullet">
+    <!-- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
+</p>
+<p>
+<h1>Accueil et index </h1>
+</p>
+<p>
+<h2>Raison d'être du site</h2>
+<p>Ces notes de cours présentent quelques bases et d'autres plus avancés en lien avec mes cours. Pour les premiers, c'est pour permettre de reprendre les quelques notions qui peuvent être un peu floues, de manière à les clarifier et qu'ils fassent partie de votre vocabulaire de base en mathématiques. Pour les plus avancés, ces notes viennent en complément de la référence su exo7. </p>
+<p>Toutes les ressources disponibles sur ce site sont disponibles dans cette page. Au risque d'être chargée, vous aurez une vision claire du contenu de ce site. 
+ </p>
+<p>Je présente et renvoie à ce contenu lors de sessions de formations pour s'approprier ces notions sous un angle à la fois académique mais avant didactique. J'espère de par mes formulations et explication littéraires fournir quelques clarifications à mes élèves et toute personne qui tomberait sur ces pages. </p>
+</p>
+<p> Ces notes sont sur GitHub pour permettre un meilleur partage avec tous mes élèves et une mise à jour plus simple que de générer de multiples PDF, les versionner, les envoyer... Cette manière de diffuser mes notes me permet la plus grande souplesse. </p>
+<h3>Pourquoi étudier les mathématiques</h3>
+<p> Parce que c'est fun! Et accessoirement, ça peut rendre riche (aussi pécuniairement!) </p>
+<p> Même si c'est vrai, blagues mise à part, leur étude est une discipline fascinante digne des livres des SF les plus élaborés et ayant la beauté des plus grandes œuvres jamais réalisées. Le tout mêlant abstraction et applications concrètes. Les fusées décollent, on sait  créer des particules subatomiques, donner une position presque n'importe où dans le monde à quelques mètres près... Et des milliers d'autres choses, qui sans les maths n'existeraient pas.  </p>
+<p> Plus sérieusement, potentiellement toute activité peut être approchée, représentée, améliorée grâce aux maths. </p>
+<p>
+</p>
+<p> Ce site utilise quelques éléments de <a href="https://github.com/nadvornix/calculus-made-easy" target="_blank"></a>ces sources</a> pour la mise en forme.  </p>
+<p> Les sources de ces pages sont sur GitHub (<a href="https://github.com/IamHenri/G1-Geometrie" target="_blank">https://github.com/IamHenri/G1-Geometrie</a>). Ca me permet de prendre en compte les remarques qui seront faites qui permettraient une meilleure compréhension des notions présentées ici plus facilement. </p>
+<p> La plupart des dessins sont fait à la main sur une tablette dédiée. L'idée est de pousser les lectrices et lecteurs à les reproduire. </p>
+<p>Quelques liens vers des ressources connexes : </p>
+<p><a href="https://www.arte.tv/fr/videos/RC-021426/voyages-au-pays-des-maths/" target="_blank">Sur Arte</a>
+</p>
+<p> 
+    <hr>
+<p class="a rotatedFloralHeartBullet">
+    <!-- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
+</p>
+<p>
+<h2>Contenu du site en détail</h2> 
+<h3>Sujets introductifs : les bases</h3>
+<ul>
+    <li><a href="chapitre-1-contexte-mathematiques-repere.php">Contexte mathématiques</a></li>
+    <li><a href="chapitre-2-les-coordonnees-cartesiennes.php">Les coordonnées cartésiennes</a></li>
+    <li><a href="chapitre-3-les-coordonnees-polaires.php">Les coordonnées polaires</a></li>
+    <li><a href="chapitre-4-les-coordonnees-dans-l-espace.php">Les coordonnées dans l'espace</a></li>
+    <li><a href="chapitre-5-les-coordonnees-cylindriques.php">Les coordonnées cylindriques</a></li>
+    <li><a href="chapitre-6-les-coordonnees-spheriques.php">Les coordonnées sphériques</a></li>
+</ul>
+<h3><a href="chapitre-7.0-les-vecteurs.php">Les vecteurs  </a></h3>
+<ul>
+    <li><a href="chapitre-7.1-les-vecteurs-presentation-intuitive-premieres-definitions.php">Premières définitions</a></li>
+    <li><a href="chapitre-7.2-les-vecteurs-coordonnees-cartesiennes-et-polaires-des-vecteurs.php">Les coordonnées</a></li>
+    <li><a href="chapitre-7.3-les-vecteurs-operation-sur-les-vecteurs.php">Opérations sur les vecteurs </a></li>
+    <li><a href="chapitre-7.4-les-vecteurs-produit-scalaire-et-comparaison-des-vecteurs.php">Le produit scalaire</a></li>
+    <li><a href="chapitre-7.5-les-vecteurs-Relation-AL-Kashi-Mediane-formule-cosinus-et-sinus.php">Al-Kashi ou la formule du cosinus (et du sinus)</a></li>
+    <li><a href="chapitre-7.6-les-vecteurs-lieux-geometriques.php">Lieux géométriques</a></li>
+    <li><a href="chapitre-7.7-les-vecteurs-TLDR.php">Les vecteurs : TL;DR</a></li>
+</ul>
+<!--  <li><a href="chapitre-8.5-element-de-geometrie-distance-point-plan.php">Distance d'un point à un plan</a></li>
+      <li><a href="chapitre-8.6-element-de-geometrie-projection-orthogonale-sur-un-plan.php">Projection orthogonlae sur un plan</a></li>
+      <li><a href="chapitre-8.7-element-de-geometrie-definir-droites-vecteurs-segments.php">Construire des droites, segments</a></li> -->
+<h3>Quelques pages sur les barycentres</h3>
+<ul>
+    <li><a href="chapitre-9-les-barycentres-et-polynomes-de-bernstein-chapitre1.php">Première approche et formules</a></li>
+    <li><a href="chapitre-9-les-barycentres-et-polynomes-de-bernstein-chapitre2.php">Théorie</a></li>
+    <li><a href="chapitre-9-les-barycentres-et-polynomes-de-bernstein-chapitre3.php">Applications</a></li>
+    <li><a href="chapitre-9-les-barycentres-et-polynomes-de-bernstein-chapitre4.php">Barycentres mouvants et introduction aux polynômes de Bernstein</a></li>
+    <li><a href="chapitre-9-les-barycentres-et-polynomes-de-bernstein-chapitre5.php">Exercices corrigés</a></li>
+</ul>
+<br>
+</p>
+<p>
+<h3>L'algèbre linéaire - TBD</h3>
+<ul>
+    <li><a href="algebre-lineaire-01.php">premier chapitre</a></li>
+</ul>
+</p>
+<p>
+<h3>Quelques pages sur des sujets unitaires</h3>
+<ul>
+    <li><a href="cours-unitaire-le-determinant-outil-fondamental-des-mathematiques.php">Le déterminant</a></li>
+    <li><a href="cours-unitaire-les-quaternions-theorie-et-application.php">Les Quaternions</a></li>
+    <li><a href="cours-unitaire-2D-3D-plan-droite-du-plan-et-de-l-espace.php">Droites et plans de l'espace version géométrie analytique ;</a></li>
+    <li><a href="le-plan-r2-l-espace-r3.php">Pourquoi on $\mathbb{R}^{2}$ pour le plan et $\mathbb{R}^{3}$ l'espace?</a></li>
+</ul>
+</p>
+<p>
+<h3>Quelques éléments de géométrie</h3>
+<ul>
+    <li> <a href="chapitre-8.1-element-de-geometrie-Distance-entre-deux-point.php">Distance entre deux points</a>;</li>
+    <li> <a href="chapitre-8.2-element-de-geometrie-Distance-point-droite.php">Distance entre un point et une droite</a>;</li>
+    <li> <a href="chapitre-8.3-element-de-geometrie-projete-orthogonal-vecteur-sur-vecteur.php">Projeté d'un vecteur sur un vecteur</a>;</li>
+    <li> <a href="chapitre-8.4-element-de-geometrie-coordonnees-projete-orthogonal-droite.php">Coordonnées du projeté orthogonal</a>;</li>
+    <li> <a href="chapitre-8.5-element-de-geometrie-distance-point-plan.php">Distance d'un point à un plan</a>;</li>
+    <li> <a href="chapitre-8.6-element-de-geometrie-projection-orthogonale-sur-un-plan.php">Projection orthogonale sur un plan</a>;</li>
+    <li> <a href="chapitre-8.7-element-de-geometrie-definir-droites-vecteurs-segments.php">Définir vecteurs, droites, segments</a>;</li>
+</ul>
+</p>
+<p>
+<h3>Analyse Vectorielle</h3>
+<ul>
+    <li><a href="analyse-vectorielle/analyse-vectorielle-chap01.php">Premières définition et champ d'application de l'analyse vectorielle</a></li>
+    <li><a href="analyse-vectorielle/analyse-vectorielle-chap02.php">Premières définition et champ d'application de l'analyse vectorielle</a></li>
+    <li><a href="analyse-vectorielle/analyse-vectorielle-chap03.php">Les opérateurs de l'analyse vectorielle</a></li>
+    <li><a href="analyse-vectorielle/analyse-vectorielle-chap04.php">Les champs scalaires et vectoriels</a></li>
+    <li><a href="analyse-vectorielle/analyse-vectorielle-chap05.php">L'opérateur Nabla</a></li>
+    <li><a href="analyse-vectorielle/analyse-vectorielle-chap06.php">TL;DR</a></li>
+</ul>
+</p>
+<p>
+<h3>Trucs et astuces de calcul</h3>
+<ul>
+    <li><a href="une-autre-methode-de-multiplication.php">Une méthode puissante de multiplication</a></li>
+</ul>
+</p>
+<br>
+<hr>
+<a href="Prologue.php">Page suivante &rarr;</a><br>
+<a href="chapitre-1-contexte-mathematiques-repere.php">Zapper le prologue &rarr;</a><br> <?php  include 'pieddepage.php';?>
